99d622a3fafc4f2e45e46bee814a178d40603c15
Quinn Yockey <qyockey@tutanota.de>
2026-07-17 10:33:53 -0400

    Add skeleton file

---
+++ Sensors/Loom_Random/Loom_Random_Skeleton.cpp
@@ -0,0 +1,19 @@
+#include "Loom_Random.h"
+
+Loom_Random::Loom_Random(Manager &man) : Module() {
+}
+
+void Loom_Random::initialize() {
+}
+
+void Loom_Random::measure() {
+}
+
+void Loom_Random::display_data() {
+}
+
+void Loom_Random::power_up() {
+}
+
+void Loom_Random::power_down() {
+}
