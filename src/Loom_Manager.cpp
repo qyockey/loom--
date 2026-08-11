2c83725bd8898ed8ca64f0bfb0e924f165f93d21
Quinn Yockey <qyockey@tutanota.de>
2026-08-11 09:14:27 -0700

    Flash builtin LED while Serial connects

--- src/Loom_Manager.cpp
+++ src/Loom_Manager.cpp
@@ -18,6 +18,8 @@ void Manager::registerModule(Module *module) {
 void Manager::beginSerial() {
     Serial.begin(BAUD_RATE);
     while (!Serial) {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        delay(100);
     }
 }
 
